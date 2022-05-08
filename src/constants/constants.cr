2575a8272544239815ff911e999a3a89fadb339c
leofigy <angel.fig@yandex.com>
2022-05-08 08:27:35 -0500

    default values

---
+++ src/constants/constants.cr
@@ -0,0 +1,18 @@
+module Constants
+    # intel values
+    SMC_KEY_CPU_TEMP     = "TC0P"
+	SMC_KEY_GPU_TEMP     = "TG0P"
+	SMC_KEY_FAN0_RPM_CUR = "F0Ac"
+    # M1 values
+    SCM_KEY_TP01 = "Tp01"
+    SCM_KEY_TP05 = "Tp05"
+    SCM_KEY_TP0D = "Tp0D"
+    SCM_KEY_TP0H = "Tp0H"
+    SCM_KEY_TP0L = "Tp0L"
+    SCM_KEY_TP0P = "Tp0P"
+    SCM_KEY_TP0X = "Tp0X"
+    SCM_KEY_TP0B = "Tp0b"
+    # M1 effecient core
+    SMC_KEY_TP09 = "Tp09"
+    SMC_KEY_TP0T = "Tp0T"
+end
